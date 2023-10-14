2c37a8e3d34a5c0a9ed12e0e1fc870b34bcb5c1f
Ismail Sadiq <sadiq6@uwindsor.ca>
2023-10-13 23:54:31 -0400

    added functionality for get all blogposts and get single blogpost apis

--- controllers/blogpostController.js
+++ controllers/blogpostController.js
@@ -3,8 +3,13 @@ const Blogpost = require('../models/Blogpost');
 // @desc        Fetch all blogposts
 //@route        GET /api/v1/blogposts
 //@access       Public
-exports.getBlogPosts = (req, res, next) => {
-  res.status(200).json({ success: true, message: 'Show all blog posts' });
+exports.getBlogPosts = async (req, res, next) => {
+  try {
+    const blogposts = await Blogpost.find();
+    res.status(200).json({ success: true, data: blogposts });
+  } catch (error) {
+    res.status(400).json({ success: false });
+  }
 };
 
 // @desc        Create a blogpost
@@ -22,10 +27,20 @@ exports.createBlogPost = async (req, res, next) => {
 // @desc        Fetch a single blogpost
 //@route        GET /api/v1/blogposts/:id
 //@access       Public
-exports.getBlogPostById = (req, res, next) => {
-  res
-    .status(200)
-    .json({ success: true, message: `Show blog post ${req.params.id}` });
+exports.getBlogPostById = async (req, res, next) => {
+  try {
+    const blogpost = await Blogpost.findById(req.params.id);
+
+    if (!blogpost) {
+      return res
+        .status(404)
+        .json({ success: true, data: 'Blogpost Not Found' });
+    }
+
+    res.status(200).json({ success: true, data: blogpost });
+  } catch (error) {
+    res.status(400).json({ success: false });
+  }
 };
 
 // @desc        Update a single blogpost
