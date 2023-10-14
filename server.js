0d39bcfc9414975d8509d8d6edf2f427a26382fe
Ismail Sadiq <sadiq6@uwindsor.ca>
2023-10-13 21:11:33 -0400

    Created blog post routes and linked it to server.js

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
+// Importing route files
+const blogposts = require('./routes/blogposts');
+
 // Load env variables
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
+
+// Mount routers to corresponding route files
+app.use('/api/v1/blogposts', blogposts)
 
 const PORT = process.env.PORT || 8000;
 
