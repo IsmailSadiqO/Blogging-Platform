9db7d6cb636a4537f4ac6bfe92fac66a498d82c2
Ismail Sadiq <sadiq6@uwindsor.ca>
2023-10-23 20:53:32 -0400

    replacing axios with redux and api slices

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,7 +1,7 @@
-export const BASE_URL =
-  process.env.NODE_ENV === 'development' ? 'http://localhost:8000' : '';
+// export const BASE_URL =
+//   process.env.NODE_ENV === 'development' ? 'http://localhost:8000' : '';
 
-// export const BASE_URL = '';
+export const BASE_URL = '';
 
 export const BLOGPOSTS_URL = '/api/v1/blogposts';
 
--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,39 +1,48 @@
 import { Row, Col } from 'react-bootstrap';
 import Blogpost from '../components/Blogpost';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-// import { useGetBlogpostsQuery } from '../slices/blogpostsApiSlice';
-// import LearningPath from '../components/LearningPath';
+// import { useEffect, useState } from 'react';
+// import axios from 'axios';
+import { useGetBlogpostsQuery } from '../slices/blogpostsApiSlice';
 // import Loader from '../components/Loader';
 // import Message from '../components/Message';
-// import Paginate from '../components/Paginate';
 // import { useParams } from 'react-router-dom';
 // import { Link } from 'react-router-dom';
-// import SearchBox from '../components/SearchBox';
 
 const HomeScreen = () => {
-  const [blogposts, setBlogposts] = useState([]);
+  // const [blogposts, setBlogposts] = useState([]);
 
-  useEffect(() => {
-    const fetchBlogposts = async () => {
-      const { data: fullData } = await axios.get('/api/v1/blogposts');
-      let blogpostData = fullData.data;
-      setBlogposts(blogpostData);
-    };
-    fetchBlogposts();
-  }, []);
+  // useEffect(() => {
+  //   const fetchBlogposts = async () => {
+  //     const { data: fullData } = await axios.get('/api/v1/blogposts');
+  //     let blogpostData = fullData.data;
+  //     setBlogposts(blogpostData);
+  //   };
+  //   fetchBlogposts();
+  // }, []);
+
+  const { data: fullData, isLoading, error } = useGetBlogpostsQuery();
+  let blogposts = fullData?.data;
+
+  console.log(blogposts);
 
   return (
     <>
-      <h1>Blogposts</h1>
-
-      <Row>
-        {blogposts.map((blogpost) => (
-          <Col key={blogpost._id} sm={12} md={6} lg={4} xl={3}>
-            <Blogpost blogpost={blogpost} />
-          </Col>
-        ))}
-      </Row>
+      {isLoading ? (
+        <h2>Loading..</h2>
+      ) : error ? (
+        <div>{error?.data?.message || error.error}</div>
+      ) : (
+        <>
+          <h1>Blogposts</h1>
+          <Row>
+            {blogposts.map((blogpost) => (
+              <Col key={blogpost._id} sm={12} md={6} lg={4} xl={3}>
+                <Blogpost blogpost={blogpost} />
+              </Col>
+            ))}
+          </Row>
+        </>
+      )}
     </>
   );
 };
